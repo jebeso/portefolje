09f234721c07574acd9823de8282bdf4b1dc9e83
Jens Bech-Sørensen <jens@bechsor.no>
2023-03-21 21:24:48 +0100

    px-2 container

--- components/container.tsx
+++ components/container.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const Container = ({ children }: Props) => {
   return (
-    <div className="container mx-auto px-3 sm:px-10 max-w-4xl">{children}</div>
+    <div className="container mx-auto px-2 sm:px-10 max-w-4xl">{children}</div>
   );
 };
 
